820832bb14f0b8746a0e34f6965d5928bc1abe08
Fernando Nicolas <fersanti2896@gmail.com>
2022-03-15 12:48:49 -0600

    Mostrando información del usuario en home.component

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators';
 import { environment } from '../../../environments/environment'
 import { Auth } from '../interfaces/auth.interface';
 
@@ -8,9 +9,19 @@ import { Auth } from '../interfaces/auth.interface';
 })
 export class AuthService {
   private baseUrl: string = environment.baseUrl;
+  private _auth: Auth | undefined; /* Almacenará el nombre que devuelve le respuesta al backend */
+
+  get auth(): Auth {
+    return { ...this._auth! }
+  }
+
   constructor(private http: HttpClient) { }
 
   login() {
-    return this.http.get<Auth>(`${this.baseUrl}/usuarios/1`);
+    return this.http.get<Auth>(`${this.baseUrl}/usuarios/1`)
+               .pipe(
+                 /* Tap hace efectos secundarios antes de pasar por el subscribe */
+                 tap(auth => this._auth = auth)
+               );
   }
 }
--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Auth } from 'src/app/auth/interfaces/auth.interface';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -12,8 +14,12 @@ import { Router } from '@angular/router';
   ]
 })
 export class HomeComponent {
+  get auth() {
+    return this.authService.auth;
+  }
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+              private authService: AuthService) { }
 
   logout() {
     /* Ir al backend */
